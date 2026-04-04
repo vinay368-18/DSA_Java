9722a0155a8cb6d0e91368cb3d12b17454e70cf2
vinay <vinay@Vinay.lan>
2026-04-04 12:38:29 +0530

    Disjoint(a,b):does a have the elements of b

---
+++ dsa/gfg/SetsInterFace/Disjoint.java
@@ -0,0 +1,29 @@
+package gfg.SetsInterFace;
+import java.util.*;
+public class Disjoint {
+
+	public static void main(String[] args) {
+		
+		int[] arr1 = {10,20,30,40,50};
+		int[] arr2 = {90,120,200,50,250};
+		System.out.println("Two arrays:");
+		System.out.print("Arr1:");
+		for(int i:arr1)
+			System.out.print(i+"  ");
+		System.out.print("\nArr2:");
+		for(int i:arr2)
+			System.out.print(i+" ");
+		System.out.println("\n\nDisjoint:"+checkDisjoint(arr1,arr2));
+	}
+
+	static boolean checkDisjoint(int[] a,int[] b) {
+		HashSet<Integer> set = new HashSet<>();
+		for(int i:a)
+			set.add(i);
+		for(int i:b) {
+			if(set.contains(i)) // if a contains b element return false
+				return false;
+		}
+		return true;
+	}
+}
