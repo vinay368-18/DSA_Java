b5dcf660caf61a7bd6a6c48c4d8ab552643d44f3
Vinay Kumar <1nt24cs122.vinayakumar@nmit.ac.in>
2026-07-22 15:07:21 +0530

    to check expression is valid or not

---
+++ dsa/gfg/Stack/ParenthesisCheck.java
@@ -0,0 +1,26 @@
+package gfg.Stack;
+import java.util.*;
+public class ParenthesisCheck {
+	public static void main(String[] args) {
+		String sg = "[()(]{}";
+		boolean r = isValid(sg);
+		System.out.println("Valid:"+r);
+	}
+	static boolean isValid(String sg) {
+		char[] ch = sg.toCharArray();
+		ArrayDeque<Character> s = new ArrayDeque<>();
+		for(char c:ch) {
+			if(c == '{' || c == '(' || c == '[') {
+				s.push(c);
+			}
+			else {
+				if(s.isEmpty())
+					return false;
+				char top = s.pop();
+				if((c=='}'&&top!='{') || (c==']'&&top!='[') || (c==')' && c=='('))
+					return false;
+			}
+		}
+		return s.isEmpty();
+	}
+}
