3c0d0b6df0569cd1d70934fd1ad6adcb3c9f85c7
Vinay Kumar <1nt24cs122.vinayakumar@nmit.ac.in>
2026-07-24 14:38:46 +0530

    Heap Sort

---
+++ dsa/gfg/PriorityQueue/heapSort.java
@@ -0,0 +1,42 @@
+package gfg.PriorityQueue;
+
+import java.util.Arrays;
+
+public class heapSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {10,5,3,8,2};
+		heapSort(arr);
+		System.out.println("Sorted Order:");
+		for(int i:arr)
+			System.out.print(i+" ");
+	}
+
+	static void heapSort(int[] arr) {
+		int n = arr.length;
+		for(int i=n/2-1;i>0;i--) 
+			heapify(arr,n,i);
+		for(int i=n-1;i>0;i--) {
+			int temp = arr[0];
+			arr[0] = arr[i];
+			arr[i] = temp;
+			heapify(arr,i,0);
+		}
+	}
+	static void heapify(int arr[],int n,int i) {
+		int largest = i;
+		int left = 2*i+1;
+		int right = 2*i+2;
+		if(right<n && arr[right]>arr[largest])
+			largest = right;
+		if(left<n && arr[left]>arr[largest])
+			largest = left;
+		if(largest!=i) {
+			int temp = arr[i];
+			arr[i] = arr[largest];
+			arr[largest] = temp;
+			heapify(arr,n,largest);
+		}
+	}
+}
