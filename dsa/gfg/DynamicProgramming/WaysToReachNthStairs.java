9b579ddd93bdf135e0f9ba5610d3ec567ffaf5ee
Vinay Kumar <1nt24cs122.vinayakumar@nmit.ac.in>
2026-07-26 21:59:20 +0530

    to count the number of ways, the person can reach the top of stairs by clibing one or two steps at a time

---
+++ dsa/gfg/DynamicProgramming/WaysToReachNthStairs.java
@@ -0,0 +1,24 @@
+package gfg.DynamicProgramming;
+import java.util.*;
+public class WaysToReachNthStairs {
+
+	public static void main(String[] args) {
+		int n;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter number of stairs:");
+		n = sc.nextInt();
+		System.out.println(countWays(n));
+		sc.close();
+	}
+	static int countWays(int n) {
+		if(n==0 || n==1)
+			return 1;
+		int[] dp = new int[n+1];
+		dp[0] = 1;
+		dp[1] = 1;
+		for(int i=2;i<=n;i++) {
+			dp[i] = dp[i-1]+dp[i-2];
+		}
+		return dp[n];
+	}
+}
